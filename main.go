44a4fb2663fe9f474b9135cea96262c952ef81a6
Armand Syahtama <armand.syahtama@gmail.com>
2018-08-21 08:53:37 -0400

    set up rudimentary ping

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	"os"
@@ -14,7 +15,7 @@ var (
 
 func main() {
 	token = getEnv("TomoyoKey")
-	if token == "exit" {
+	if token == "" {
 		os.Exit(3)
 	}
 	discord, err := discordgo.New("Bot " + token)
@@ -22,11 +23,12 @@ func main() {
 	user, err := discord.User("@me")
 	errorCheck("Error retrieving account", err)
 	botID = user.ID
+	discord.AddHandler(pingCommand)
 	discord.AddHandler(func(session *discordgo.Session, ready *discordgo.Ready) {
 		err = session.UpdateStatus(0, "Tomoyo Ready!")
 		errorCheck("Error attempting to set status", err)
 		servers := session.State.Guilds
-		fmt.Printf("Tomoyo is running on %d servers", len(servers))
+		fmt.Printf("Tomoyo is running on %d servers \n", len(servers))
 	})
 
 	err = discord.Open()
@@ -36,12 +38,28 @@ func main() {
 	<-make(chan struct{})
 }
 
+func pingCommand(session *discordgo.Session, message *discordgo.MessageCreate) {
+	if user := message.Author; user.ID == botID || user.Bot {
+		fmt.Println("Yeah no, this'll lead to spam")
+		return
+	}
+	if string(message.Content[0]) != commandPrefix {
+		return
+	}
+	unpackedMessage, _ := json.Marshal(message)
+	fmt.Println(string(unpackedMessage))
+	_, err := session.ChannelMessageSend(message.ChannelID, "I'm Alive")
+	if err != nil {
+		return
+	}
+}
+
 func getEnv(key string) string {
 	if value, ok := os.LookupEnv(key); ok {
 		return value
 	}
 	fmt.Println("No token was found")
-	return "exit"
+	return ""
 }
 
 func errorCheck(msg string, err error) {
