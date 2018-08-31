fa9e83494c4c7aa707a5fbbc7026d1019fa71655
Armand Syahtama <armand.syahtama@gmail.com>
2018-08-31 08:16:15 -0400

    fixed bugs with json marshalling

--- anilist/anilistTypes.go
+++ anilist/anilistTypes.go
@@ -19,17 +19,17 @@ type CharacterImage struct {
 }
 
 type Character struct {
-	ID          int            `json:"id"`
-	Name        CharacterName  `json:"name"`
-	Image       CharacterImage `json:"image"`
-	Description string         `json:"description"`
-	SiteURL     string         `json:"siteUrl"`
-	// Media       MediaConnection `json:"media"`
+	ID          int             `json:"id"`
+	Name        CharacterName   `json:"name"`
+	Image       CharacterImage  `json:"image"`
+	Description string          `json:"description"`
+	SiteURL     string          `json:"siteUrl"`
+	Media       MediaConnection `json:"media"`
 }
 
-// type MediaConnection struct {
-// 	Nodes []Media `json:"nodes"`
-// }
+type MediaConnection struct {
+	Nodes []Media `json:"nodes"`
+}
 
 type MediaTitle struct {
 	Romaji  string `json:"romaji"`
@@ -71,7 +71,7 @@ type Media struct {
 }
 
 type CharacterSearchResults struct {
-	Character []Character `json:"character"`
+	Characters []Character `json:"characters"`
 }
 
 type AnimeSearchResults struct {
--- anilist/searchCharacter.go
+++ anilist/searchCharacter.go
@@ -8,7 +8,7 @@ import (
 //SearchCharacter [title]
 func SearchCharacter(title string) interface{} {
 	query := `query ($search: String) {
-				characterSearchResults: Page {
+		characterSearchResults: Page {
 					characters(search: $search, sort: SEARCH_MATCH){
 						id
 						name {
@@ -23,14 +23,23 @@ func SearchCharacter(title string) interface{} {
 						}
 						description
 						siteUrl
+						media {
+							nodes {
+								id
+								title {
+									romaji
+									english
+									native
+								}
+							}
+						}
 					}
 				}
 			}`
-	fmt.Println(query)
 	variables := map[string]string{"search": title}
 	queryResults := runQuery(query, variables)
 	if data, ok := queryResults.(Data); ok {
-		characterSearchResults := data.CharacterSearchResults.Character
+		characterSearchResults := data.CharacterSearchResults.Characters
 		fmt.Printf("chartac: %v \n", len(characterSearchResults))
 		for i, characterSearchResult := range characterSearchResults {
 			characterSearchResult.Description = misc.StripHTML(characterSearchResult.Description)
