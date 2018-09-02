73e6fdc33e37e60d40857aedec40ceef08a46a1b
Armand Syahtama <armand.syahtama@gmail.com>
2018-09-02 12:49:48 -0400

    set up user stat types

--- anilist/anilistTypes.go
+++ anilist/anilistTypes.go
@@ -75,12 +75,46 @@ type Media struct {
 	CoverImage   Image      `json:"coverImage"`
 }
 
+type StatusDistribution struct {
+	Status string `json:"status"`
+	Amount int    `json:"amount"`
+}
+
+type UserActivityHistory struct {
+	Date   int `json:"date"`
+	Amount int `json:"amount"`
+	Level  int `json:"level"`
+}
+
+type ScoreDistribution struct {
+	Score  int `json:"score"`
+	Amount int `json:"amount"`
+}
+
+type ListScoreStats struct {
+	MeanScore         int `json:"meanScore"`
+	StandardDeviation int `json:"standardDeviation"`
+}
+
+type UserStats struct {
+	WatchedTime             int                   `json:"watchedTime"`
+	ChaptersRead            int                   `json:"chaptersRead"`
+	ActivityHistory         []UserActivityHistory `json:"activityHistory"`
+	AnimeStatusDistribution []StatusDistribution  `json:"animeStatusDistribution"`
+	MangaStatusDistribution []StatusDistribution  `json:"mangaStatusDistribution"`
+	AnimeScoreDistribution  []ScoreDistribution   `json:"animeScoreDistrubtion"`
+	MangaScoreDistribution  []ScoreDistribution   `json:"mangaScoreDistribution"`
+	AnimeListScores         ListScoreStats        `json:"animeListScores"`
+	ManagaListScores        ListScoreStats        `json:"mangaListScores"`
+}
+
 type User struct {
 	ID         int        `json:"id"`
 	Name       string     `json:"name"`
 	About      string     `json:"about"`
 	Avatar     Image      `json:"avatar"`
 	Favourites Favourites `json:"favourites"`
+	Stats      UserStats  `json:"stats"`
 	SiteURL    string     `json:"siteUrl"`
 	UpdatedAt  int        `json:"updatedAt"`
 }
